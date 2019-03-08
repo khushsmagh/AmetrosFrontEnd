3ec8abd9d91816283dfc559e52e64a2ac048d797
Brijesh Patel <b.patel405@mybvc.ca>
2019-03-08 10:42:16 -0700

    serving static files with node on port 8080

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+//start server
+const server = app.listen(8080, () => {
+    console.log(`Express running -> PORT ${server.address().port}`);
+});
+
+//start serving static files
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/landing-page/html/landing-page.html');
+});
