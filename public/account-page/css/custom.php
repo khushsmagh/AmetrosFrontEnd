69ca52ab84a21c6ecdbc4d228af0ba2d1d2ef85a
Anderson Viana <a.resendeviana683@mybvc.ca>
2019-03-28 12:14:55 -0600

    Using hardcoded CSS to define color schema.

--- public/account-page/css/custom.php
+++ public/account-page/css/custom.php
@@ -3,7 +3,7 @@
 
 	require_once('../../includes/colors.class.php');
 
-	$partnerID = 1; // this value will be retreived from the DB based on the logged in user
+	$partnerID = 2; // this value will be retreived from the DB based on the logged in user
 
 	$colors = new Colors($partnerID);
 	
--- public/includes/colors.class.php
+++ public/includes/colors.class.php
@@ -9,18 +9,31 @@ class Colors {
 	protected $color4;	
 	
 	function __construct($partnerID) {
-
-		if($partnerID == 1){
-			$this->color1 = "#22f";
-			$this->color2 = "#77f";		
-			$this->color3 = "#333";
-			$this->color4 = "#777";
-		}else{
-			$this->color1 = "#f22";
-			$this->color2 = "#f77";		
-			$this->color3 = "#333";
-			$this->color4 = "#777";			
+		switch($partnerID) {
+			case 1:
+				$this->color1 = "rgb(245, 0, 39)";
+				$this->color2 = "rgb(132, 46, 178)";		
+				$this->color3 = "#333";
+				$this->color4 = "#777";
+				break;
+			case 2:
+				$this->color1 = "rgb(0, 0, 0)";
+				$this->color2 = "rgb(245, 0, 39)";		
+				$this->color3 = "#333";
+				$this->color4 = "#777";		
+				break;
 		}
+		// if($partnerID == 1){
+		// 	$this->color1 = "#22f";
+		// 	$this->color2 = "#77f";		
+		// 	$this->color3 = "#333";
+		// 	$this->color4 = "#777";
+		// }else{
+		// 	$this->color1 = "#f22";
+		// 	$this->color2 = "#f77";		
+		// 	$this->color3 = "#333";
+		// 	$this->color4 = "#777";			
+		// }
 	}
 	
 	public function color1() {
