d67ebfe7bd718accb70d901a2c500b25c566a6b8
hai_do_488 <h.do488@mybvc.ca>
2019-04-08 10:07:42 -0600

    adding function to fetch api for partner information in landing page

--- public/landing-page/js/common.js
+++ public/landing-page/js/common.js
@@ -1,3 +1,47 @@
+// References for html elements
+const partnerName = document.getElementById('partner-name');
+const partnerDescription = document.getElementById('partner-description');
+const partnerLogo = document.getElementById('partner-logo');
+const partnerBanner = document.getElementById('banner-background');
+const wholePage = document.getElementsByTagName('body');
+
+window.addEventListener('load', function() {
+    displayPartnerInfo();
+});
+
+function displayPartnerInfo() {
+    /** 
+     * Examplr of response obj after fetch api for
+     * partner information
+     * partner = {
+     *  "name" : "Bow Valley",
+     *  "url" : "bvc",
+     *  "description" : "lorem lorem lorem",
+     *  "logo_url" : "https://bvc.logo.png",
+     *  "backgroundColor" : "#ff076",
+     *  "textColor" : "#000",
+     *  "theme" : "theme1"
+     * }
+    */
+    const URL = "https://jsonplaceholder.typicode.com/users";
+    
+    fetch(URL)
+        .then(response => {
+            return response.json();
+        })
+        .then(jsonData => {
+            // mapping data to html elements for partner info
+            partnerName.innerHTML = jsonData.name;
+            partnerDescription.innerHTML = jsonData.description;
+            partnerLogo.setAttribute('src', jsonData.logo_url);
+            partnerBanner.style.backgroundColor = jsonData.backgroundColor;
+            partnerBanner.style.color = jsonData.textColor;
+            // apply partner current theme to whole page
+            wholePage.setAttribute('class', jsonData.theme);
+        })
+        .catch(err => console.log(err));
+}
+
 function displayCartItemsNumber() {
     // This function is to display current
     // sims within the user's cart
