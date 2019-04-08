4f35de98348c671a15617b5323b0448ed2d7b199
Brijesh Patel <b.patel405@mybvc.ca>
2019-04-07 23:50:37 -0600

    test api route created to collect styles from api

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
 
-//start server
+//body parser middleware to get params from requests
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
 
+//set port
 app.set('port', process.env.PORT || 8000);
 
 //start serving static files
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/styles/bvc', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json({
+        styles: {
+            color1: "rgba(131,58,180,1)",
+            color2: "rgba(253,29,29,1)",
+            color3: "rgba(252,176,69,1)",
+            textColor : "black"
+        },
+        Status: "success"
+    });
 });
 
 app.listen(app.get('port'), () => {
