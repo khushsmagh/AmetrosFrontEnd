e4c4d9cb287bd00881d522f54ce3527dbd1cd1f8
Brijesh Patel <b.patel405@mybvc.ca>
2019-04-16 01:55:29 -0600

    batch transaction added to update all sims atomic

--- server.js
+++ server.js
@@ -82,6 +82,74 @@ app.get('/sims/:partnerUrl', (req, res) => {
 /**
  * Post API to update partner information and style information.
  */
+// app.post('/partner/:partnerUrl', (req, res) => {
+//     const partnerInfo = req.body;
+//     const partnerUrl = req.params.partnerUrl;
+
+//     //get id of the document
+//     db.collection('Partners').where("Url", "==", partnerUrl).get()
+//         .then(snapshot => {
+//             let data = snapshot.docs.filter((doc) => (doc.data().Url == partnerUrl));
+//             if (data.length > 0) {
+//                 let docId = data[0].id;
+
+//                 //check url is already used
+//                  db.collection('Partners').where("Url", "==", partnerInfo.url).get()
+//                     .then(aSnapshot => {
+//                         //update partner info if url is unique
+//                         if (aSnapshot.docs.length == 0) {
+//                             //update info in partner collection
+//                             db.collection("Partners")
+//                                 .doc(docId)
+//                                 .update({
+//                                     Name: partnerInfo.name,
+//                                     Description: partnerInfo.description,
+//                                     Url: partnerInfo.url,
+//                                     LogoUrl: partnerInfo.logo,
+//                                     Color1: partnerInfo.styles.color1,
+//                                     Color2: partnerInfo.styles.color2,
+//                                     Color3: partnerInfo.styles.color3,
+//                                     Color4: partnerInfo.styles.color4,
+//                                     Color5: partnerInfo.styles.color5,
+//                                     Color6: partnerInfo.styles.color6
+//                                 })
+//                                 .then(() => {
+//                                     res.status(200).json({
+//                                         Guidance: "Request Complete.",
+//                                         Status: "success"
+//                                     });
+//                                 })
+//                                 .catch(error => {
+//                                     console.error(error);
+//                                     res.status(200).json({
+//                                         Guidance: "Error Occured.",
+//                                         Status: "Failure"
+//                                     });
+//                                 });
+//                         } else {
+//                             res.status(400).json({
+//                                 Guidance: "URL not unique. Please provide different url to update.",
+//                                 Status: "Failure"
+//                             });
+//                         }
+//                     })
+//                     .catch();
+//             } else {
+//                 res.status(400).json({
+//                     Guidance: "Wrong Url Provided to update.",
+//                     Status: "Failure"
+//                 });
+//             }
+//         })
+//         .catch((error) => {
+//             console.error(error);
+//             res.status(400).json({
+//                 Guidance: "Wrong Url Provided to update.",
+//                 Status: "Failure"
+//             });
+//         });
+// });
+
 app.post('/partner/:partnerUrl', (req, res) => {
     const partnerInfo = req.body;
     const partnerUrl = req.params.partnerUrl;
@@ -94,38 +162,48 @@ app.post('/partner/:partnerUrl', (req, res) => {
                 let docId = data[0].id;
 
                 //check url is already used
-                 db.collection('Partners').where("Url", "==", partnerInfo.url).get()
+                db.collection('Partners').where("Url", "==", partnerInfo.url).get()
                     .then(aSnapshot => {
                         //update partner info if url is unique
                         if (aSnapshot.docs.length == 0) {
-                            //update info in partner collection
-                            db.collection("Partners")
-                                .doc(docId)
-                                .update({
-                                    Name: partnerInfo.name,
-                                    Description: partnerInfo.description,
-                                    Url: partnerInfo.url,
-                                    LogoUrl: partnerInfo.logo,
-                                    Color1: partnerInfo.styles.color1,
-                                    Color2: partnerInfo.styles.color2,
-                                    Color3: partnerInfo.styles.color3,
-                                    Color4: partnerInfo.styles.color4,
-                                    Color5: partnerInfo.styles.color5,
-                                    Color6: partnerInfo.styles.color6
-                                })
-                                .then(() => {
-                                    res.status(200).json({
-                                        Guidance: "Request Complete.",
-                                        Status: "success"
+                            
+                            let batch = db.batch();
+                            let docRef = db.collection("Partners").doc(docId);
+
+                            //update info in partner doc ref
+                            batch.update(docRef, {
+                                Name: partnerInfo.name,
+                                Description: partnerInfo.description,
+                                Url: partnerInfo.url,
+                                LogoUrl: partnerInfo.logo,
+                                Color1: partnerInfo.styles.color1,
+                                Color2: partnerInfo.styles.color2,
+                                Color3: partnerInfo.styles.color3,
+                                Color4: partnerInfo.styles.color4,
+                                Color5: partnerInfo.styles.color5,
+                                Color6: partnerInfo.styles.color6
+                            });
+
+                            db.collection('Simulations').where("partnerUrl", "==", partnerUrl).get()
+                                .then((aSnap) => {
+                                    let data = aSnap.docs;
+                                    data.forEach(sim  => {
+                                        let simRef = db.collection("Simulations").doc(sim.id);
+                                        batch.update(simRef, { partnerUrl: partnerInfo.url});
                                     });
+                                    batch.commit().then(() => {
+                                            res.status(400).json({
+                                                Guidance: "Information has been updated.",
+                                                Status: "Success"
+                                            });
+                                        }).catch(() => {
+                                            res.status(400).json({
+                                                Guidance: "Error while updating content. Please try again later.",
+                                                Status: "Failure"
+                                            });
+                                        });
                                 })
-                                .catch(error => {
-                                    console.error(error);
-                                    res.status(200).json({
-                                        Guidance: "Error Occured.",
-                                        Status: "Failure"
-                                    });
-                                });
+
                         } else {
                             res.status(400).json({
                                 Guidance: "URL not unique. Please provide different url to update.",
@@ -149,6 +227,7 @@ app.post('/partner/:partnerUrl', (req, res) => {
             });
         });
 });
+
 
 app.listen(app.get('port'), () => {
     console.log('Express running ->' + app.get('port'));
